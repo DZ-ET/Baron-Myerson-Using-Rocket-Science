28d139b12b192e233b658bd6102f575546e7cf45
Dihan Zou <dihan32768@outlook.com>
2025-09-15 22:18:55 -0400

    Update VV.m

--- VV.m
+++ VV.m
@@ -1,6 +1,8 @@
 function V = VV(z,par)
 
-GrossVal = cumtrapz(par.v(end:-1:1) .* par.g) * par.d; % v represents Q
+% GrossVal = cumtrapz(par.v(end:-1:1) .* par.g) * par.d; % v represents Q
+
+GrossVal = cumtrapz(par.P(par.v)) * par.d;
 
 V = interp1(par.v, GrossVal, z, 'spline', 'extrap');
 
